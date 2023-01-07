8ce0172390c6dda14d9a62f744524e5d76488e69
George Chacko <chackoge@illinois.edu>
2023-01-07 18:48:22 +0000

    minor annotation

--- analysis/find_trees.py
+++ analysis/find_trees.py
@@ -12,6 +12,8 @@ parser.add_argument('-i', type=str, required=True)
 parser.add_argument('-o', type=str, required=True)
 parser.add_argument('-b', type=int, required=True)
 args = parser.parse_args()
+
+print("*** Begin ***")
 print(args)
 
 input_graph=args.g
@@ -19,25 +21,26 @@ input_clustering=args.i
 bound = args.b
 cnames=['node_id','cluster_id']
 
-# Read in clustering file
+print("Read in clustering file")
 l_df = pd.read_table(input_graph,header=None,names=cnames)
 
-# Convert to dictionary (takes too long and would benefit from speedup)
+print("Converting clustering df to dict")
 l_dict = dict(l_df.groupby('cluster_id')['node_id'].apply(lambda x: x.tolist()))
 
-# load input graph to clustering
+print("Loading in graph used as input to clustering")
 elr = nk.graphio.EdgeListReader("\t", 0, continuous = False)
 g = elr.read(input_graph)
 g = nk.graphtools.toUndirected(g) # may not be necessary
 print(nk.graphtools.size(g))
 print("Loaded graph of size {}".format(nk.graphtools.size(g)))
 
-# make a working dict (reduces teh amount of computation
+print("Make a working dict (reduces the amount of computation)"
 working_dict  = {k:v for k, v in l_dict.items() if len(v)> bound}
 
 # convert node labels to ids
 node_map = elr.getNodeMap()
 
+print("Looping through working dict")
 # loop through dict
 datalist = list()
 for key in working_dict:
@@ -51,10 +54,12 @@ for key in working_dict:
     print("Completed {}".format(key))
     datalist.append(node_tuple)
 
+print("Converting datalist to df")
 # Convert to df and write to file
 df2 = pd.DataFrame(datalist, columns =['cluster_id', 'node_count', 'edge_count'])
 df2.to_csv("datalist_{}_{}.tsv".format(args.o,args.b), sep="\t")
 print("All Done")
+    
 
 
 
