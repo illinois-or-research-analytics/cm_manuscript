b92308d7becfa1eca9261f1eed194658caadb035
George Chacko <chackoge@users.noreply.github.com>
2023-01-17 11:55:50 -0600

    generic update

--- analysis/sample_oc.R
+++ analysis/sample_oc.R
@@ -2,7 +2,8 @@
 # taking 10% from each decile after clusters below 11 nodes are removed. 
 
 # usage "Rscript sample_oc.R <name of input clustering file> 
-# which should be in tsv without headers
+# use the output of subset_graph_nonnetworkit_treestar.R
+# which has the string treestarcounts.tsv suffixed in the filename
 
 library(data.table)
 rm(list=ls())
@@ -17,26 +18,19 @@ if (length(args)==0) {
 }
 
 x <- fread(args[1])
-y <- x[,.N,by='V2'][N>10]
-z <- quantile(y$N,probs=seq(0,1,by=0.1))
+x <- x[type=='non_tree']
 
-for(i in 1:(length(unname(z))-1)) {
-print(i)
-temp = y[N >= z[i] & N <= z[i+1]]
-sample_size <- ceiling(10*dim(temp)[1]/100)
-dec_sample <- sample(temp$V2, sample_size)
-write.csv(dec_sample,file=paste0('dec_sample_',i,'.csv'),row.names=FALSE)
+qvec <- quantile(x$node_count,probs=seq(0.1,1,by=0.1))
+
+i=1
+sample <- x[node_count <= qvec[i]][,sample(cluster_id,ceiling(5*length(cluster_id)/100))]
+i=1
+while (i <= (length(qvec)-1)) {
+print(paste(i,i+1))
+s <- x[node_count >= qvec[i] & node_count <= qvec[i+1]][,sample(cluster_id,ceiling(5*length(cluster_id)/100))]
+sample <- union(sample,s)
+i=i+1
 }
 
-file_vec <- list.files(pattern="*.csv")
-file_list <- list()
-for (i in 1:length(file_vec)){
-file_list[[i]] <- fread(file_vec[i])
-}
-
-flatlist_df <- rbindlist(file_list)
-flatlist_df <- unique(flatlist_df)
-
-oc_sample <-x[V2 %in% flatlist_df$x]
-write.table(oc_sample,file=paste0('sample_leiden_',args[1]),sep='\t',row.names=F,col.names=F)
+write.table(sample,file=paste0('dec_sample_',args[1],i,'.csv'),sep='\t',row.names=F,col.names=F)
 
