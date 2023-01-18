b439c40096658c98a8ea6ec5c6df9c265bbf56b1
George Chacko <chackoge@illinois.edu>
2023-01-18 23:18:09 +0000

    updated for simpler logic. 4% total sample

--- analysis/sample_oc.R
+++ analysis/sample_oc.R
@@ -1,11 +1,18 @@
 # script that samples from a large Leiden clustering
 # taking 10% from each decile after clusters below 11 nodes are removed. 
+# updated to incorporate a simpler strategy to avoid the 'identical bin' problem
+# that results in oversampling.
 
-# usage "Rscript sample_oc.R <path_to_annotated_cluster_file> <path_to_original clustering> <output_identifier_string> 
+# Revised strategy
+# Sample 1 % of the population from above the 3rd quartile and
+# 3 % from below the third quartile
+
+
+# usage "Rscript sample_oc.R <path_to_original clustering> <path_to_annotated_cluster_file> 
 # use the output of subset_graph_nonnetworkit_treestar.R
 # which has the string treestarcounts.tsv suffixed in the filename
 # **** Example: on odesa when run from /data2/oc_sampling
-# Rscript /data1/chackoge/repos/cm_manuscript/analysis/sample_oc.R  ./oc_leiden.5_annotated_treestarcounts.tsv ../open_citations/oc_leiden.5.tsv  oc_leiden.5.
+# Rscript /data1/chackoge/repos/cm_manuscript/analysis/sample_oc.R ../open_citations/oc_leiden.5.tsv  oc_leiden.5_annotated_treestarcounts.tsv 
 # ****
 library(data.table)
 rm(list=ls())
@@ -24,7 +31,7 @@ print(args[1])
 print(args[2])
 print(args[3])
 
-x <- fread(args[1])
+x <- fread(args[2])
 # filter out trees and stars
 x <- x[type=='non_tree']
 
@@ -32,25 +39,21 @@ x <- x[type=='non_tree']
 set.seed(12345)
 
 # construct quantiles
-qvec <- quantile(x$node_count,probs=seq(0.1,1,by=0.1))
+q3 <- quantile(x$node_count,probs=0.75)
 
-# sample
-i=1
-sample <- x[node_count <= qvec[i]][,sample(cluster_id,ceiling(5*length(cluster_id)/100))]
-i=1
-while (i <= (length(qvec)-1)) {
-print(paste(i,i+1))
-s <- x[node_count >= qvec[i] & node_count <= qvec[i+1]][,sample(cluster_id,ceiling(5*length(cluster_id)/100))]
-sample <- union(sample,s) # eliminates duplicates
-i=i+1
-}
+s1 <- x[node_count >= q3][,sample(cluster_id,ceiling(length(cluster_id)/100))]
+s2 <- x[node_count < q3][,sample(cluster_id,ceiling(3*length(cluster_id)/100))]
+print(length(s1))
+print(length(s2))
+sample <- union(s1,s2) 
 
 #import original clustering
-original_clustering <- fread(args[2])
+original_clustering <- fread(args[1])
 cmready_clustering <- original_clustering[V2 %in% sample]
 
-write.table(sample,file=paste0('dec_sample_',args[1],'.tsv'),sep='\t',row.names=F,col.names=F)
-write.table(cmready_clustering,file=paste0('cm_ready_sample_',args[1],'.tsv'),sep='\t',row.names=F,col.names=F)
+write.table(sample,file=paste0('dec_sample_',args[1]),sep='\t',row.names=F,col.names=F)
+write.table(cmready_clustering,file=paste0('cm_ready_sample_',args[1]),sep='\t',row.names=F,col.names=F)
+
 
 
 
