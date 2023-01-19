2df01daa3f164e875639bf6fd5cae5b4889345b9
George Chacko <chackoge@illinois.edu>
2023-01-19 01:11:05 +0000

    changed sampling approach

--- analysis/sample_oc.R
+++ analysis/sample_oc.R
@@ -8,12 +8,13 @@
 # 3 % from below the third quartile
 
 
-# usage "Rscript sample_oc.R <path_to_original clustering> <path_to_annotated_cluster_file> 
+# usage "Rscript sample_oc.R <original clustering> <annotated_cluster_file> 
 # use the output of subset_graph_nonnetworkit_treestar.R
 # which has the string treestarcounts.tsv suffixed in the filename
 # **** Example: on odesa when run from /data2/oc_sampling
 # Rscript /data1/chackoge/repos/cm_manuscript/analysis/sample_oc.R ../open_citations/oc_leiden.5.tsv  oc_leiden.5_annotated_treestarcounts.tsv 
 # ****
+
 library(data.table)
 rm(list=ls())
 
@@ -21,7 +22,7 @@ args = commandArgs(trailingOnly=TRUE)
 
 if (length(args)==0) {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
-} else if (length(args)==1) {
+} else if (length(args)==2) {
   # default output file
   args[3] = "default.tsv"
 }
@@ -45,14 +46,21 @@ s1 <- x[node_count >= q3][,sample(cluster_id,ceiling(length(cluster_id)/100))]
 s2 <- x[node_count < q3][,sample(cluster_id,ceiling(3*length(cluster_id)/100))]
 print(length(s1))
 print(length(s2))
-sample <- union(s1,s2) 
+sample <- union(s1,s2)
+
+print("***")
+print(length(sample))
+print("***")
 
 #import original clustering
 original_clustering <- fread(args[1])
 cmready_clustering <- original_clustering[V2 %in% sample]
 
-write.table(sample,file=paste0('dec_sample_',args[1]),sep='\t',row.names=F,col.names=F)
-write.table(cmready_clustering,file=paste0('cm_ready_sample_',args[1]),sep='\t',row.names=F,col.names=F)
+write.table(sample,file=paste0('dec_sample_',args[2]),sep='\t',row.names=F,col.names=F)
+write.table(cmready_clustering,file=paste0('cm_ready_sample_',args[2]),sep='\t',row.names=F,col.names=F)
+
+
+
 
 
 
