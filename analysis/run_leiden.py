7ce37ca2d4eec40df57dc89803334c9913715152
ytabatabaee <yasamin.tabatabaee@gmail.com>
2023-02-21 14:20:29 -0600

    adding num iterations and seed to leiden script

--- analysis/run_leiden.py
+++ analysis/run_leiden.py
@@ -9,12 +9,19 @@ if __name__ == "__main__":
                         help='network edge-list path')
     parser.add_argument('-r', metavar='resolution', type=float, required=True,
                         help='resolution parameter (gamma)')
+    parser.add_argument('-i', metavar='iterations', type=int, required=False,
+                        help='number of iterations in Leiden')
+    parser.add_argument('--seed', metavar='seed', type=int, required=False,
+                        help='random seed in Leiden')
     parser.add_argument('-o', metavar='output', type=str, required=True,
                         help='output membership path')
     args = parser.parse_args()
 
     net = igraph.Graph.Load(args.n, format='edgelist', directed=False)
-    partition = leidenalg.find_partition(net, leidenalg.CPMVertexPartition, resolution_parameter=args.r)
+    partition = leidenalg.find_partition(net, leidenalg.CPMVertexPartition,
+                                         resolution_parameter=args.r,
+                                         n_iterations=args.i if args.i else 2,
+                                         seed=args.seed if args.seed else 1234)
     with open(args.o, "w") as f:
         for n, m in enumerate(partition.membership):
             f.write(f"{n}\t{m}\n")
